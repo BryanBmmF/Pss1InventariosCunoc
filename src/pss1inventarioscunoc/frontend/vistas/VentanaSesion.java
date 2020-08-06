31ef79f394fbc7f523d225fee0030c8dc21087ff
fabricioRG <fabricio21rg@gmail.com>
2020-08-06 10:25:48 -0600

    imagen de sesion actualizada

--- src/pss1inventarioscunoc/frontend/vistas/VentanaSesion.java
+++ src/pss1inventarioscunoc/frontend/vistas/VentanaSesion.java
@@ -40,8 +40,6 @@ public class VentanaSesion extends javax.swing.JFrame {
     private void initComponents() {
 
         jPanel1 = new javax.swing.JPanel();
-        jLabel7 = new javax.swing.JLabel();
-        jLabel6 = new javax.swing.JLabel();
         jLabel1 = new javax.swing.JLabel();
         jPanel2 = new javax.swing.JPanel();
         btnIniciar = new javax.swing.JButton();
@@ -62,17 +60,7 @@ public class VentanaSesion extends javax.swing.JFrame {
 
         jPanel1.setLayout(new org.netbeans.lib.awtextra.AbsoluteLayout());
 
-        jLabel7.setFont(new java.awt.Font("Dialog", 0, 46)); // NOI18N
-        jLabel7.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel7.setText("CUNOC");
-        jPanel1.add(jLabel7, new org.netbeans.lib.awtextra.AbsoluteConstraints(90, 280, -1, 50));
-
-        jLabel6.setFont(new java.awt.Font("Dialog", 1, 46)); // NOI18N
-        jLabel6.setForeground(new java.awt.Color(255, 255, 255));
-        jLabel6.setText("INVENTARIOS");
-        jPanel1.add(jLabel6, new org.netbeans.lib.awtextra.AbsoluteConstraints(20, 210, -1, 70));
-
-        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pss1inventarioscunoc/frontend/media/panel-principal.png"))); // NOI18N
+        jLabel1.setIcon(new javax.swing.ImageIcon(getClass().getResource("/pss1inventarioscunoc/frontend/media/inventarios-pantalla.jpg"))); // NOI18N
         jPanel1.add(jLabel1, new org.netbeans.lib.awtextra.AbsoluteConstraints(0, 0, 390, 550));
 
         jPanel2.setBackground(new java.awt.Color(255, 255, 255));
@@ -258,8 +246,6 @@ public class VentanaSesion extends javax.swing.JFrame {
     private javax.swing.JLabel jLabel3;
     private javax.swing.JLabel jLabel4;
     private javax.swing.JLabel jLabel5;
-    private javax.swing.JLabel jLabel6;
-    private javax.swing.JLabel jLabel7;
     private javax.swing.JPanel jPanel1;
     private javax.swing.JPanel jPanel2;
     private javax.swing.JSeparator jSeparator1;
