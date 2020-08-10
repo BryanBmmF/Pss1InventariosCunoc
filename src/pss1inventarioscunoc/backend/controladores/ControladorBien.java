b3c733a5632d367ec11b5c83260b5cd244197e4c
Jesfrin Gonzalez <JesfrinGnZ>
2020-08-10 10:57:39 -0600

    Avance en vonsulta de bienes, y traslados

--- src/pss1inventarioscunoc/backend/controladores/ControladorBien.java
+++ src/pss1inventarioscunoc/backend/controladores/ControladorBien.java
@@ -3,6 +3,8 @@
  */
 package pss1inventarioscunoc.backend.controladores;
 
+import java.sql.Timestamp;
+import java.util.ArrayList;
 import pss1inventarioscunoc.backend.dao.implementaciones.ImplementacionBien;
 import pss1inventarioscunoc.backend.dao.interfaces.BienDAO;
 import pss1inventarioscunoc.backend.pojos.Bien;
@@ -29,4 +31,49 @@ public class ControladorBien {
         return bienDao.registrar(bien);
     }
     
+    /**
+     * Regresa todos los bienes del sistema
+     * @return 
+     */
+    public ArrayList<Bien> consultarBienes(){
+        return (ArrayList<Bien>)bienDao.recuperarLista();
+    }
+    
+    /**
+     * Regresa todos los bienes por DONACION del sistema,
+     * null si existiera un error
+     * @return 
+     */
+    public ArrayList<Bien> consultarBienDonacion(){
+        return (ArrayList<Bien>)bienDao.consultarBienDonacion();
+    }
+
+    /**
+     * Regresa todos los bienes por TRASLADO del sistema,
+     * null si existiera un error
+     * @return 
+     */
+    public ArrayList<Bien> consultarBienTraslado(){
+        return (ArrayList<Bien>)bienDao.consultarBienTraslado();
+    }
+   /**
+     * Regresa todos los bienes por COMPRA del sistema,
+     * null si existiera un error
+     * @return 
+     */
+    public ArrayList<Bien> consultarBienCompra(){
+        return (ArrayList<Bien>)bienDao.consultarBienCompra();
+    }
+    /**
+     * Se traslada un bien ya existente
+     * @param cur
+     * @param fecha
+     * @param autorizacion
+     * @param seccion
+     * @param persona_que_recibio
+     * @return 
+     */
+    public boolean trasladarBien(String cur, Timestamp fecha, String autorizacion, String seccion, String persona_que_recibio){
+        return bienDao.trasladarBien(cur, fecha, autorizacion, seccion, persona_que_recibio);
+    }
 }
--- src/pss1inventarioscunoc/backend/dao/interfaces/ProveedorDAO.java
+++ src/pss1inventarioscunoc/backend/dao/interfaces/ProveedorDAO.java
@@ -16,12 +16,12 @@ public interface ProveedorDAO extends CRUD<Proveedor>{
     public ArrayList<Proveedor> buscarProveedoresPorNombre(String nombre);
     public ArrayList<Proveedor> buscarProveedoresPorDireccion(String direccion);
     public ArrayList<Proveedor> buscarProveedorPorDescripcion(String descripcion);
-    
+    public boolean modificarProveedor(Proveedor proveedor);
     /*Querys a utilizar*/
     public static final String BUSQUEDA_POR_NOMBRE="SELECT * FROM PROVEEDOR WHERE nombre_Proveedor LIKE ?";
     public static final String BUSQUEDA_POR_DIRECCION="SELECT * FROM PROVEEDOR WHERE direccion LIKE ?";
     public static final String BUSQUEDA_POR_DESCRIPCION="SELECT * FROM PROVEEDOR WHERE descripcion LIKE ?";
-
+    public static final String MODIFICAR_PROVEEDOR="UPDATE PROVEEDOR SET nombre_Proveedor=?,nit=?,direccion=?,descripcion=?,correoElectronico=? WHERE id_Proveedor=?";
 
     //CRUD
     public static final String INSERTAR_PROVEEDOR = "INSERT INTO PROVEEDOR(nombre_Proveedor,nit,direccion,descripcion,correoElectronico) VALUES(?,?,?,?,?)";
