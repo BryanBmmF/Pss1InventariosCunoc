34a01ddc8b356688e9cdbac167bf8b33a2ffd252
fabricioRG <fabricio21rg@gmail.com>
2020-08-12 12:12:52 -0600

    documentacion controladorEncargado

--- src/pss1inventarioscunoc/backend/controladores/ControladorEncargado.java
+++ src/pss1inventarioscunoc/backend/controladores/ControladorEncargado.java
@@ -24,6 +24,12 @@ public class ControladorEncargado {
         this.encargadoDAO = new ImplementacionEncargado();
     }
     
+    /**
+     * Metodo encargado de registrar un encargado
+     * Si no existen errores en la base de datos se devuelve un dialogo con un
+     * informe de exito, de lo contrario se informará un error
+     * @param encargado 
+     */
     public void registrar(Encargado encargado){
         if (encargadoDAO.registrar(encargado)) {
             JOptionPane.showMessageDialog(null, "Encargado registrado correctamente", "Informacion", JOptionPane.INFORMATION_MESSAGE);
@@ -32,11 +38,12 @@ public class ControladorEncargado {
         }
     }
     
-    public void actualizarEncargadosValidacionEncargados(ValidacionEncargados ve){
-        ve.getListaEncargadosObsr().clear();
-        ve.getListaEncargadosObsr().addAll(obtenerEncargadosActuales());
-    }
-    
+    /**
+     * Retorna una Lista de objetos Encargado
+     * Se utiliza la implementacion de encargado para obtener todos los encargados
+     * registrados en el sistema
+     * @return 
+     */
     public List<Encargado> obtenerEncargadosActuales(){
         return encargadoDAO.recuperarLista();
     }
@@ -45,6 +52,25 @@ public class ControladorEncargado {
      * Metodos pertenecientes a GUI ValidacionEncargados
      */
     
+    /**
+     * Recibe como primer parametro la instancia de la interfaz (vista) a manejar
+     * Método encargado de actualizar la lista de Encargados después de una
+     * accion en la vista
+     * @param ve 
+     */
+    public void actualizarEncargadosValidacionEncargados(ValidacionEncargados ve){
+        ve.getListaEncargadosObsr().clear();
+        ve.getListaEncargadosObsr().addAll(obtenerEncargadosActuales());
+    }
+    
+    /**
+     * Recibe como primer parametro la instancia de la interfaz (vista) a manejar
+     * Metodo encargado de validar que los campos ingresados en la vista no esten
+     * vacios, llamando a otra funcion para validarlos
+     * Se registra el nuevo encargado por medio de la llamada a una funcion
+     * Por utlimo, se limpian los campos de la vista
+     * @param ve 
+     */
     public void agregarButtonValidacionEncargados(ValidacionEncargados ve){
         List<String> campos = new LinkedList<>();
         campos.add(ve.getDpiTextField().getText());
@@ -60,6 +86,12 @@ public class ControladorEncargado {
         }
     }
     
+    /**
+     * Recibe como primer parametro la instancia de la interfaz (vista) a manejar
+     * Metodo encargado de limpiar los campos en la vista en que se esta trabajando
+     * colocando como texto por defecto la variable EMPTY_TEXT
+     * @param ve 
+     */
     public void limpiarButtonValidacionEncargados(ValidacionEncargados ve){
         ve.getDpiTextField().setText(EMPTY_TEXT);
         ve.getNombreTextField().setText(EMPTY_TEXT);
@@ -76,6 +108,13 @@ public class ControladorEncargado {
         
     }
     
+    /**
+     * Recibe como primer parametro una lista de objetos String
+     * Retorna un valor verdadero si alguno de los campos en la lista se
+     * encuentra vacia, de lo contrario retornara falso
+     * @param campos
+     * @return 
+     */
     private boolean existenCamposVacios(List<String> campos){
         for (String campo : campos) {
             if(campo.trim().isEmpty()){
